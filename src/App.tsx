b0a0637a8159d5772f1605f036e2400b18dc9224
Ivan Monastyrev <ikloster@yandex.ru>
2020-03-26 22:15:52 +0300

    app: add test waiting

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,22 @@ import Test from '@/store/modules/test'
 export default class App extends tsx.Component<{}> {
   private test = 'hello world!!!!'
   private testModule!: Test
-  public created() {
+  public async created() {
+    this.$wait.start('app is load')
     this.testModule = getModule(Test, this.$store)
-    this.testModule.updateTest('new test')
+    await this.testModule.updateTest('new test')
+    this.$wait.end('app is load')
   }
   public render(): Vue.VNode {
     return (
-      <div>
+      <v-wait for="app is load">
+        <template slot="waiting"> Waiting...</template>
         <HelloWorld
           msg={this.testModule.test}
           num={1}
           obj={{ test: this.test }}
         />
-      </div>
+      </v-wait>
     )
   }
 }
