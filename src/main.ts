7efe387a76872099a8a54167d1e09fb46caf2c08
Ivan Monastyrev <ikloster@yandex.ru>
2020-03-26 22:14:55 +0300

    main: add vue wait

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueWait from 'vue-wait'
 
+Vue.use(VueWait)
 Vue.config.productionTip = false
-
+const wait = new VueWait({ useVuex: true })
 new Vue({
   router,
   store,
+  wait,
   render: h => h(App)
 }).$mount('#app')
