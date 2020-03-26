1564f9be1dc52a4d7f094c38f6a18cbd3c255204
Ivan Monastyrev <ikloster@yandex.ru>
2020-03-26 22:17:44 +0300

    app: add IS_LOAD

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import HelloWorld from '@/components/HelloWorld'
 import * as tsx from 'vue-tsx-support'
 import { getModule } from 'vuex-module-decorators'
 import Test from '@/store/modules/test'
-
+const IS_LOAD = 'app is load'
 @Component({
   name: 'App',
   components: {
@@ -14,14 +14,14 @@ export default class App extends tsx.Component<{}> {
   private test = 'hello world!!!!'
   private testModule!: Test
   public async created() {
-    this.$wait.start('app is load')
+    this.$wait.start(IS_LOAD)
     this.testModule = getModule(Test, this.$store)
     await this.testModule.updateTest('new test')
-    this.$wait.end('app is load')
+    this.$wait.end(IS_LOAD)
   }
   public render(): Vue.VNode {
     return (
-      <v-wait for="app is load">
+      <v-wait for={IS_LOAD}>
         <template slot="waiting"> Waiting...</template>
         <HelloWorld
           msg={this.testModule.test}
